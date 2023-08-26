a4d53fdc628580bde92863170d74d5506c9628e0
Alan Kara <alankara.dev@gmail.com>
2023-08-26 10:40:47 +1000

    Choose a demo

--- app/choose.jsx
+++ app/choose.jsx
@@ -8,25 +8,28 @@ export default function Choose() {
   return (
     <div
       className={classNames(
-        'demo border overflow-hidden w-full h-full flex flex-wrap flex-col items-center justify-center text-center bg-white/20 rounded-lg'
+        'demo border overflow-hidden w-full h-full flex flex-wrap flex-col items-center justify-center text-center bg-white/50 rounded-lg'
       )}
     >
       <span
         className={classNames(
-          'text-5xl mb-10 text-rose-900',
+          'text-5xl scroll-mb-10 text-rose-900',
           dancingScript.className
         )}
       >
         Choose a Demo
+        <br />⇢
       </span>
 
-      <span className="text-md font-semibold text-lime-900">
-        Bomonti Sydney
-      </span>
+      <div className="flex flex-col">
+        <span className="text-md font-semibold text-lime-900">
+          Bomonti Sydney
+        </span>
 
-      <span className="text-sm">44 Market St., NSW 2000</span>
+        <span className="text-sm">44 Market St., NSW 2000</span>
 
-      <span className="text-sm">+61 491 663 609</span>
+        <span className="text-sm">+61 491 663 609</span>
+      </div>
     </div>
   );
 }
