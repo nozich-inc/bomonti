e3c8b0a7d03a4fd742b9c7ac519603ff790b3cbf
Alan Kara <alankara.dev@gmail.com>
2023-08-28 21:52:22 +1000

    Choose dark

--- app/choose.jsx
+++ app/choose.jsx
@@ -8,12 +8,12 @@ export default function Choose() {
   return (
     <div
       className={classNames(
-        'demo cursor-default p-2 overflow-hidden w-full h-full flex flex-wrap flex-col items-center justify-center text-center bg-white rounded-lg'
+        'demo cursor-default p-2 overflow-hidden w-full h-full flex flex-wrap flex-col items-center justify-center text-center bg-black/30 rounded-lg'
       )}
     >
       <span
         className={classNames(
-          'text-5xl scroll-mb-10 text-rose-900',
+          'text-5xl scroll-mb-10 text-white',
           dancingScript.className
         )}
       >
@@ -22,7 +22,7 @@ export default function Choose() {
 
       <span
         className={classNames(
-          'text-3xl mt-2 scroll-mb-6 text-rose-950/50',
+          'text-3xl mt-2 scroll-mb-6 text-white/50',
           dancingScript.className
         )}
       >
@@ -30,21 +30,21 @@ export default function Choose() {
       </span>
 
       <span
-        className={classNames('text-5xl mt-2 mb-4 scroll-mb-10 text-rose-900')}
+        className={classNames('text-5xl mt-2 mb-4 scroll-mb-10 text-white')}
       >
         ⇢
       </span>
 
       <div className="flex flex-col">
-        <span className="text-md font-semibold text-lime-900">
+        <span className="text-md font-semibold text-lime-300">
           Bomonti Sydney
         </span>
 
-        <span className="text-xs text-lime-900/50">
+        <span className="text-xs text-lime-300/50">
           44 Market St., NSW 2000
         </span>
 
-        <span className="text-xs text-lime-900/50">+61 491 663 609</span>
+        <span className="text-xs text-lime-300/50">+61 491 663 609</span>
       </div>
     </div>
   );
